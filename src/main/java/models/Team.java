511e460318d83ddfa0ad84f9065b26933e30fdc1
shy <shyamal.punekar@gmail.com>
2017-08-11 08:59:01 -0700

    Add first test to veriy object instantiates correctly

---
+++ src/main/java/models/Team.java
@@ -0,0 +1,16 @@
+package models;
+
+/**
+ * Created by ssaxe1 on 8/11/17.
+ */
+public class Team {
+    private String name;
+
+    public Team (String name) {
+        this.name =name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
---
+++ src/test/java/models/TeamTest.java
@@ -0,0 +1,23 @@
+package models;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by ssaxe1 on 8/11/17.
+ */
+public class TeamTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @Test
+    public void Team_instantiatesCorrectly_true() {
+        Team testTeam = new Team("IT Crowd");
+        assertEquals(true, testTeam instanceof Team);
+    }
+
+}
